7ef873b15cee3b6cc70a9e6c69c4d4e711d1dddc
Alec L. Robitaille <robit.alec@gmail.com>
2021-01-06 17:59:39 -0330

    grab minimal example
    from: https://github.com/wlandau/targets-minimal

---
+++ _targets.R
@@ -0,0 +1,25 @@
+library(targets)
+library(tarchetypes)
+source("R/functions.R")
+options(tidyverse.quiet = TRUE)
+
+tar_option_set(packages = c('tidyverse'))
+
+list(
+  tar_target(
+    raw_data_file,
+    "data/raw_data.csv",
+    format = "file"
+  ),
+  tar_target(
+    raw_data,
+    read_csv(raw_data_file, col_types = cols())
+  ),
+  tar_target(
+    data,
+    raw_data %>%
+      mutate(Ozone = replace_na(Ozone, mean(Ozone, na.rm = TRUE)))
+  ),
+  tar_target(hist, create_plot(data)),
+  tar_target(fit, lm(Ozone ~ Wind + Temp, data))
+)
